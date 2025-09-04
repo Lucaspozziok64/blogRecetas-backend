73438d07a00ad4f6fdb17c854c430b54c7303b88
Lucasfi <lukafigueroa64@gmail.com>
2025-09-04 20:23:54 -0300

    Cree funcion test para probar rutas y controlador con postman

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import recetasRoutes from "./recetas.routes.js"
+import usuariosRoutes from "./usuarios.routes.js"
 
 const router = new Router()
 
 router.use('/recetas', recetasRoutes)
+router.use('/usuarios', usuariosRoutes)
 
 export default router;
