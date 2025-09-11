31ddd1b1ae7ad3f3b44a5b5a0cc8df08fdcfa3dd
Lucasfi <lukafigueroa64@gmail.com>
2025-09-11 20:37:19 -0300

    Logre validar el login email, password para admin

--- src/controllers/usuario.controllers.js
+++ src/controllers/usuario.controllers.js
@@ -28,3 +28,28 @@ export const crearUsuario = async (req, res) => {
     res.status(500).json({ mensaje: 'Error al crear el usuario' })
   }
 }
+
+export const login = async (req, res) => {
+  try {
+    // Aqui debo verificar si el email existe en la bd, sino msj error
+    const { email, password } = req.body;
+    const usuarioExistente = await Usuario.findOne({ email });
+    if(!usuarioExistente) {
+      return res.status(400).json({ mensaje: 'No se encontro el usuario' });
+    }
+
+    // Tambien debo verificar el password con bcrypt, si no es la misma msj error
+    const passwordVerificado = bcrypt.compareSync(password, usuarioExistente.password);
+    if(!passwordVerificado) {
+      return res.status(400).json({ mensaje: 'Credenciales incorrectas' });
+    }
+
+    //Aqui debo generar token 
+
+    // Aqui debo enviar una respuesta al front si es login es exitoso
+    res.status(200).json({ mensaje: 'Login exitoso', nombreUsuario: usuarioExistente.nombreUsuario })
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: 'Error al loguear usuario' })
+  }
+}
